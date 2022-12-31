1b415a4ca2b0f2c0e9cc644fb904e603a6ca170a
Damika <damikaanupama@gmail.com>
2022-12-31 12:16:10 +0530

    Add tests

---
+++ __test__/.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /users', () => {
+  it('should return a list of users', async () => {
+    const response = await request(app).get('/api/v1/user');
+    console.log(response);
+    expect(response.status).toBe(200);
+  });
+});
+
+describe('GET /users/:id', () => {
+  it('should return a single user', async () => {
+    const response = await request(app).get('/api/v1/user/1');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('user');
+  });
+});
