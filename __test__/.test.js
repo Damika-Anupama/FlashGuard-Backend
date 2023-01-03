385d057bacd640c12619359da52b6a922e64308c
Damika <damikaanupama@gmail.com>
2023-01-03 13:49:40 +0530

    Change file structure

--- __test__/.test.js
+++ __test__/.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("../src/app");
 
 describe("POST /users", () => {
   it("should create a new user", async () => {
